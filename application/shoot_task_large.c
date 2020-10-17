13222a001caae96cdabce10788e75e0554a18388
Adan <azurerinrin@gmail.com>
2020-10-17 15:08:41 -0600

    Task initialization + first layer motor packaging for speed sucessful

--- application/shoot_task_large.c
+++ application/shoot_task_large.c
@@ -13,18 +13,44 @@
 #include "referee_system.h"
 #include "hero_cmd.h"
 #include "shoot_task_large.h"
-
 //Timer necessary
 #include "tim.h"
-
 #include "stm32f4xx_hal.h"
 
+void set_large_fric_speed(uint16_t input_speed){
+//Input between 1000-2000, 1000 for no spin, 2000 for fastest speed
+//Set speed for left wheel
+	__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_4,input_speed);
 
+//Set speed for right wheel
+}
 
 
 void shoot_task_large(void const *argument){
+	///System variables
+	 uint32_t period = osKernelSysTick();
+	 
+	//Initialize the PWM ports (start PWM and give initialization signal)
+			HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_4);
+		//HAL_Delay(1750);
+		__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_4,2000);
+		  osDelayUntil(&period, 2000);
+		__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_4,1000);
+		  osDelayUntil(&period, 1750);
+	//Finish initilization of ports
+	  uint16_t speed=0;
     while(1){
-        HAL_Delay(1000);
-        HAL_GPIO_TogglePin(LED_R_GPIO_Port, LED_R_Pin);   
+			//If condition is okay, then shoot large task should set the friction wheels at the same input speed, will only worry about left wheel for now
+			while (speed<2000){
+			set_large_fric_speed(speed++);
+			osDelayUntil(&period,5);
+			}
+			while (speed>1000){
+			set_large_fric_speed(speed--);
+			osDelayUntil(&period,5);
+		  }   
     }
 }
+
+
+
